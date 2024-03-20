d60f9e490703dc829ea3c9eae6a4349e203a84f2
mohammadariful20 <arifulz848@gmail.com>
2024-03-21 03:45:35 +0600

    project setup and navbar done

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import iconProfile from '../../../public/icons/Frame.svg'
+import SearchForm from "../Search/Search";
+
+export default function Header() {
+  return (
+    <div>
+      <div className="navbar bg-base-100 text-white">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            ><li><a>Home</a></li>
+            <li><a>Recipes</a></li>
+            <li><a>About</a></li>
+            <li><a>Search</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Recipe Calories</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>Home</a></li>
+            <li><a>Recipes</a></li>
+            <li><a>About</a></li>
+            <li><a>Search</a></li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+            <SearchForm></SearchForm>
+          <img src={iconProfile} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
